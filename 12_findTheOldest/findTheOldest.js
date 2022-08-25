e574c19051b079557807419d59bf1eda6696badc
Manjindergit <manjindergrewal1997@gmail.com>
2022-08-24 23:08:58 -0700

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let person=obj[0];
+   // console.log(person.yearOfDeath);
 
+    for (let index = 0; index < obj.length; index++) {
+        if((person.yearOfDeath-person.yearOfBirth)<(obj[index].yearOfDeath-obj[index].yearOfBirth)){
+            person=obj[index];
+            
+        }
+        
+    }
+    console.log(person.name);
+
+    return person;
 };
 
 // Do not edit below this line
