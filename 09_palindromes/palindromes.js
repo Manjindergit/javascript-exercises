ba0f57d93ee3ad3740322b526b64556947a2a0bd
Manjindergit <manjindergrewal1997@gmail.com>
2022-08-24 22:42:40 -0700

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    console.log(a.split("").reverse().join(""));
+    if(a===a.split("").reverse().join("")){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    let fib=1;
+    let lastFib=1;
+
+    if(n<0){
+        return 'OOPS';
+    }
+
+    else if(n===1 || n===2){
+        return fib;
+    }
+
+    
+    
+    else{
+        for (let index = 3; index <=n; index++)
+         {
+            let temp=lastFib;
+            lastFib=lastFib+fib;
+            fib=temp;
+            
+            }
+            return lastFib;
+    }
 };
 
 // Do not edit below this line
