983b3e6e9a044d306bb4a9bd837c6e2e75671a93
Manjindergit <manjindergrewal1997@gmail.com>
2022-08-24 22:50:08 -0700

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(obj) {
+    let arr=[];
 
+    for (let index = 0; index < obj.length; index++) {
+        arr.push(obj[index].title);
+        
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
